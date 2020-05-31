7eed15da439d9a3b76b7a67a3cb636e2e1801e30
Shannon Trainor <sltrainor@outlook.com>
2020-05-31 13:05:28 -0400

    added pseudocoding

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ function init() {
 }
 
 init();
+
+
+
+//packages: fs package, path, inquirer
+//questions: 
+    //github username
+    // email
+    // url for project
+    // project name
+    // description
+    // what kind of license (list of options)
+        //choices key
+    // command - attach default answer (npm i will be assigned)
+    // command run to run tests (default answer npm test) 
+        // default key
+// function - fs write to file
+    //1. file name writing to (ReadMe.md)
+    //2. what we are writing to file
+        //call function to organize ReadME
+        // respond to function with what we are writing to file
+    
+//create array of questions
+    //pass array inside inquirer.promt
+    //.then
+// pass string to name file
+//pass function for responses to questions to gen Readme
+//return final product
+//console.log(data)
+//writeFileSync 
