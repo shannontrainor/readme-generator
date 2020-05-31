e1d0610f8b5977eed3e65a6077774f8e22702543
Shannon Trainor <sltrainor@outlook.com>
2020-05-31 14:43:36 -0400

    added data attributes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,26 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description 
+  ${data.description}
+
+## Installation
+  ${data.installation}
+
+## Usage
+  ${data.usage}
+
+## Contributors
+  ${data.contributors}
+
+## Testing
+  ${data.test}
 `;
+
+
+
+
+
 }
 
 module.exports = generateMarkdown;
